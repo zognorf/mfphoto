eeb84435a851cb8c56ed901c59ff8875698242db
Matthew Froggatt <froggatt.me@gmail.com>
2016-06-25 14:57:51 -0400

    Updated packagejson

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -7,4 +7,5 @@ app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, function() {
   console.log('Server started');
+  console.log('listening on port', PORT);
 });
